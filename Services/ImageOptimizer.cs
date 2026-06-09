09a247c2c7b2e61be145125271c2515c1212b89c
rodrigueztrach <rronny045@gmail.com>
2026-06-08 20:58:27 -0600

    feat: add ImageOptimizer service with jpeg, webp and png encoding

---
+++ Services/ImageOptimizer.cs
@@ -0,0 +1,34 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Webp;
+
+namespace ImageCLI.Services;
+
+public static class ImageOptimizer
+{
+    public static async Task OptimizeAsync(FileInfo input, FileInfo output, int quality)
+    {
+        using var image = await Image.LoadAsync(input.FullName);
+        var ext = output.Extension.ToLower();
+
+        if (ext == ".jpg" || ext == ".jpeg")
+        {
+            var encoder = new JpegEncoder { Quality = quality };
+            await image.SaveAsJpegAsync(output.FullName, encoder);
+        }
+        else if (ext == ".webp")
+        {
+            var encoder = new WebpEncoder { Quality = quality };
+            await image.SaveAsWebpAsync(output.FullName, encoder);
+        }
+        else
+        {
+            var encoder = new PngEncoder
+            {
+                CompressionLevel = PngCompressionLevel.BestCompression
+            };
+            await image.SaveAsPngAsync(output.FullName, encoder);
+        }
+    }
+}
