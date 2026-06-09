0fe29aff211aa3eb1e2706aa62447a9ac307bd73
rodrigueztrach <rronny045@gmail.com>
2026-06-08 20:03:27 -0600

    add a register commands avaliable and run on terminal

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using ImageCLI.Commands;
+
+var rootCommand = new RootCommand("ImageCLI - Optimizador y conversor de imágenes");
+
+rootCommand.AddCommand(OptimizeCommand.Build());
+rootCommand.AddCommand(ConvertCommand.Build());
+rootCommand.AddCommand(InfoCommand.Build());
+
+return await rootCommand.InvokeAsync(args);
